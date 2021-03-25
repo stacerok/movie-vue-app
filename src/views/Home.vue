a735399a0962ce5cb79fbcd19e7af29b72fdab56
Stacie Taitague <stacie.taitague@gmail.com>
2021-03-24 21:44:34 -0400

    links frontend movie-vue-app to backend movie-app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,88 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <h2>Add a Movie.</h2>
+    <p>
+      Title:
+      <input type="text" placeholder="Movie Title" v-model="newMovieTitle" />
+      <br />
+    </p>
+    <p>
+      Plot:
+      <input type="text" placeholder="Movie Plot" v-model="newMoviePlot" />
+      <br />
+    </p>
+    <p>
+      Year:
+      <input type="text" placeholder="Movie Year" v-model="newMovieYear" />
+      <br />
+    </p>
+    <p>
+      Genre:
+      <input type="text" placeholder="Movie Genre" v-model="newMovieGenre" />
+      <br />
+      <br />
+
+      <button v-on:click="createMovie()">Add Movie</button>
+    </p>
+
+    <div>
+      <div v-for="movie in movies" v-bind:key="movie.id">
+        <h3>{{ movie.title }}</h3>
+        {{ movie.plot }}
+        <br />
+        {{ movie.year }}
+        <br />
+        {{ movie.genre_name }}
+      </div>
+    </div>
   </div>
 </template>
+<style></style>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "My Movie Database",
+      movies: [],
+      newMovieTitle: "",
+      newMoviePlot: "",
+      newMovieYear: "",
+      newMovieGenre: "",
+    };
+  },
+  created: function () {
+    this.indexMovies();
+  },
+  methods: {
+    indexMovies: function () {
+      axios.get("/api/movies").then((response) => {
+        this.movies = response.data;
+        console.log("All Movies:", this.movies);
+      });
+    },
+    createMovie: function () {
+      console.log("Adding a Movie!");
+      var params = {
+        title: this.newMovieTitle,
+        plot: this.newMoviePlot,
+        year: this.newMovieYear,
+        genre: this.newMovieGenre,
+      };
+      axios
+        .post("/api/movies", params)
+        .then((response) => {
+          console.log("Successfully added movie!", response.data);
+          this.movies.push(response.data);
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+          this.errors = error.response.data.errors;
+        });
+    },
   },
 };
 </script>
